0a45b6f7b160bc95b7952e6d2c057a0605229cf0
Paskal Sitepu <rmnscnce@ya.ru>
2023-09-06 00:53:07 +0700

    drivers: Integrate kprobe-less KernelSU for the android-4.14 kernel
    This adds the KernelSU repository as a Git submodule, and implements
    the KernelSU safe mode support
    
    Signed-off-by: Paskal Sitepu <rmnscnce@ya.ru>
    Signed-off-by: Anymore Project <anymore.dev13@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -354,6 +354,13 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return error;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			        int *flags);
+#endif
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+				int *flags);
+
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -361,6 +368,8 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  */
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
@@ -368,6 +377,10 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+
+#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
